49813609aa0d193de823e4730d9d7c6f3580a120
Tu Nguyen <tuinfor@ymail.com>
2022-06-04 12:21:00 -0400

    basic decription on the readmefile

--- server.js
+++ server.js
@@ -2,22 +2,10 @@ const express = require('express');
 const session = require('express-session');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
