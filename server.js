eaa8af0b5d6eca3d2a98f65fd8666c1b510749b2
Charnay Birton <coding26fig@gmail.com>
2022-06-09 21:02:32 -0400

    checking into main branch

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+//const session = require("express-session");
 
-const sequelize = require('./config/connection');
+//const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log("Now listening"));
+// });
+
+app.listen(PORT, () => console.log("Now listening"));
